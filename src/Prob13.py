eabd92c46c5933136d4416a12a1491874d4e1ffe
Adam Beagle <abeagle88@gmail.com>
2013-04-08 02:21:53 -0400

    Add: Problems 13 and 18
    Also accompanying resource files

---
+++ src/Prob13.py
@@ -0,0 +1,30 @@
+"""
+PROJECT EULER
+Problem 13
+
+Work out the first ten digits of the sum of the
+one-hundred 50-digit numbers in ../res/Prob13_largenums.txt
+
+
+Author: Adam Beagle
+"""
+
+from os import path
+from Timer import Timer
+
+################################################################################
+def Prob13(filename):
+    with open(filename, 'r') as f:
+        _sum = sum((int(line) for line in f))
+
+    return str(_sum)[:10]
+
+
+################################################################################
+if __name__ == '__main__':
+    try:
+        with Timer() as timer:
+            filename = path.join('..', 'res', 'Prob13_largeNums.txt')
+            print 'Answer: ' + str(Prob13(filename))
+    finally:
+        print 'Time: %.5fs' % timer.Interval
---
+++ src/Prob18.py
@@ -0,0 +1,50 @@
+"""
+PROJECT EULER
+Problem 18
+
+Find the maximum total from top to bottom of
+the triangle in ../res/Prob18_triangle.txt
+
+
+Author: Adam Beagle
+"""
+
+from os import path
+from Timer import Timer
+
+################################################################################
+def MaxTriangleSum(tri, h, r=0, c=0, currSum=0, maxSum=0):
+    """
+    Returns maximum sum obtainable by travelling from
+    top to bottom of triangle tri.
+    """
+    if r < h - 1:
+        currSum += tri[r][c]
+        
+        maxSum = MaxTriangleSum(tri, h, r + 1, c + 1, currSum, maxSum)
+        maxSum = MaxTriangleSum(tri, h, r + 1, c, currSum, maxSum)
+        
+    else:
+        maxSum = max(currSum + tri[r][c], maxSum)
+
+    return maxSum
+
+#-----------------------------------------------------------------------------
+def Prob18(filename):
+    triangle = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            triangle.append([int(n) for n in line.split(' ')])
+            
+    return MaxTriangleSum(triangle, len(triangle))
+
+        
+################################################################################
+if __name__ == '__main__':
+    try:
+        with Timer() as timer:
+            filename = path.join('..', 'res', 'Prob18_triangle.txt')
+            print 'Answer: ' + str(Prob18(filename))
+    finally:
+        print 'Time: %.5fs' % timer.Interval
